76f9e4404aede8fd423652178895362ad851947e
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-02 12:40:45 -0300

    Tipo de dados

---
+++ Conhecendo a linguagem/tipo_de_dados.py
@@ -0,0 +1,63 @@
+'''
+
+Tipos de dados que temos disponibilizado em Python
+
+    - Texto -> str
+    - Numerico -> int,float,complex
+    - Sequencia -> list,tuple,complex
+    - Mapa -> dict -> chave : valor
+    - Coleção -> set,fronzenset
+    - Booleano -> bool
+    - Binário -> bytes,bytearray,memoryviwe
+
+'''
+
+'''
+
+Tipos Numericos 
+
+    Inteiros (int)
+        - precisão ilimitada .
+        - Ex : 1, 10 , 100 , -1 ,-10 , -100 ... 999120101
+
+    Ponto Flutuante (float)
+        - usados para representar numeros racionais .
+        - Ex : 1.5 , -10.534 , 0.76 , 999992,1011
+
+'''
+
+'''
+
+Tipós Booleano (bool)
+
+    Usado para representar verdadeiro ou falso 
+        - 1 -> Verdadeiro
+        - 0 -> Falso
+
+'''
+
+'''
+
+String (str)
+
+    String ou cadeia de caracter é usada para representar valores alfanumericos
+        - Ex : "Python",'Python',"p"
+
+'''
+
+# ---> Inteiro
+print (10 + 1)
+int()
+
+# ---> Ponto Flutuante
+print (10.5 + 1.5)
+float()
+
+# ---> Booleano
+print(True)
+print(False)
+bool()
+
+# ---> String
+print("Python")
+str()
