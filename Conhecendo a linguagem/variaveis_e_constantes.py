e9b42f55e24692aca00ed2bdc6cebed87a1dafe0
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-02 15:56:43 -0300

    Variaveis e Constantes

---
+++ Conhecendo a linguagem/variaveis_e_constantes.py
@@ -0,0 +1,43 @@
+'''
+
+Oque são ?
+
+    Variaveis 
+        - Armazena valores
+        - O valor pode se alterar no meio do percuso
+        - Não precisamos definir o tipo da variavel (tipagem dinamica)
+
+        - Ex : age -> 23
+        - Ex : name -> 'Guilherme'
+
+    Constantes -> Declaradas com letras maiusculas
+        - Armazena valor
+        - Nasce com um valor e não pode ser alterado
+
+        - Ex = PASTA_RAIZ = '/home/guilherme/Documents/Python'
+        - Ex = ESTADOS = [
+            'SP',
+            'RJ',
+            'MJ',
+        ]
+
+
+Boas Praticas 
+
+    - todo espaço -> '_'
+    - nomes sugestivos
+    - nome de constantes tudo em maiusculo
+    
+'''
+
+nome = 'Guilherme'
+idade = 23
+
+print(nome,idade)
+
+nome = 'Hernandez'
+
+print(nome)
+
+LIMITE_SAQUE_DIARIO = 1000
+ESTADOS_DISPONIVEIS = ['SP','RJ','MG','SC','BA']
