d182261705bfe553a546d1f24e43ef3ba704caa1
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-02 13:18:56 -0300

    Aprendendo modo interativo

---
+++ Conhecendo a linguagem/modo_interativo.py
@@ -0,0 +1,29 @@
+'''
+
+Modo interativo
+
+    Possibilita o desenvolvedor a escrever codigo , e ver o resultado na hora.
+
+        - Escrever no terminal -> python -> Para sair usamos exit()
+        - Ou executar o script com a flag -i 
+
+'''
+
+'''
+
+Função dir()
+
+    Ela retorna a lista de modulos no escopo local atual.
+        - Caso eu forneça dir(100) -> Retorna os modulos que o objetivo usa
+
+'''
+
+'''
+
+Função help()
+
+    Retorna um sistema de ajuda integrado, é possivel fazer busca no modo interativo ou informa por parametro qual o nome do modulo , função , classe , metodo ou variavel
+        - Ex : help()
+        - Ex : help(100)
+
+'''
