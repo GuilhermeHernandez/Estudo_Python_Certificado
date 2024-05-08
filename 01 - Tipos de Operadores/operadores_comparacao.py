1821afcf26825c355847c502303ff47ca51befe7
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-08 10:49:48 -0300

    Operadores de comparação

---
+++ 01 - Tipos de Operadores/operadores_comparacao.py
@@ -0,0 +1,17 @@
+'''
+
+Operadores de comparação 
+    - ver se variaveis são igual, diferente , maior , menor entre outras
+    - (== , != , > ou >= , < ou <=)
+
+'''
+
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
