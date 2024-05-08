9d58817eb7614e1d44dd1025c47c195c80626eab
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-08 10:46:32 -0300

    Aprendendo sobre operadores

---
+++ 01 - Tipos de Operadores/operadores_aritmeticos.py
@@ -0,0 +1,33 @@
+'''
+
+Conhecendo operadores aritmeticos 
+    -   Executa função matematicas
+    -  ( + , - , * , / , // -> Div inteira , % , ** )
+    -   Precendencia definida na metematica padrão 
+
+'''
+
+produto_1 = 10 
+produto_2 = 20
+
+print(produto_1 + produto_2 + 3.5)
+
+print(produto_1 - produto_2 - 3.5)
+
+print(produto_1 / produto_2)
+
+print(produto_1 // produto_2)
+
+print(produto_1 * produto_2)
+
+print(produto_1 % produto_2)
+
+print(produto_1 ** produto_2)
+
+x = 10 + 5 * 4
+
+print(x)
+
+x = (10 + 5) * 4
+
+print(x)
