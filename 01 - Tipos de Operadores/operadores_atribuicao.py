8d3ff78d19b8a27bad526218bca2145d9605ab0d
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-08 10:58:24 -0300

    Operadores de atribuição

---
+++ 01 - Tipos de Operadores/operadores_atribuicao.py
@@ -0,0 +1,32 @@
+'''
+
+Atribuição
+
+    agrega o valor para uma variavel
+    - ( = , += , -= , /= , // , %= , **=)
+
+'''
+
+saldo = 500
+
+print(saldo)
+
+saldo = 200
+
+print(saldo)
+
+saldo += 100
+
+print(saldo)
+
+saldo -= 100
+
+print(saldo)
+
+saldo *= 100
+
+print(saldo)
+
+saldo /= 2
+
+print(saldo)
