8ed924db5c9b2b4245df763abeeacb918204c3a7
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-09 10:25:36 -0300

    Operador de identidade

---
+++ 01 - Tipos de Operadores/operadores_identidade.py
@@ -0,0 +1,13 @@
+'''
+
+é aquele que usamos para saber se algum valor é certo objeto
+
+'''
+
+curso_python = 'Curso de Python'
+nome_curso = curso_python
+saldo , limite = 200 , 200
+
+print(curso_python is nome_curso)
+print(curso_python is not nome_curso)
+print(saldo is limite)
