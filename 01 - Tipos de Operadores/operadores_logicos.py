2284cc8b7aab78682f5b3b7a1f89020cb483287e
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-09 10:21:10 -0300

    Operadores logicos

---
+++ 01 - Tipos de Operadores/operadores_logicos.py
@@ -0,0 +1,20 @@
+'''
+
+Operadores logico
+
+    - and -> tudo precisa ser True
+    - or -> uma das variaveis precisa ser True
+
+'''
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
