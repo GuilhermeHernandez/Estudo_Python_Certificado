181cf022dec64bb19aa3c93c60c4cbaa84e17479
Ti - Ear - Guilherme.Batista (Guilherme Hernandez Batista) <guilherme.batista@confirpsul01.local>
2024-05-09 10:40:06 -0300

    Estudando sobre identação

---
+++ 02 - Estrutura Condiciona e de Repeticao/indentacao_e_blocos.py
@@ -0,0 +1,15 @@
+'''
+
+Atraves da identação determinamos um bloco de comando
+    - 4 espaços em branco (TAB)
+
+'''
+
+def sacar(valor):
+
+    saldo = 5000
+
+    if saldo >= valor:
+        print("Valor sacado !")
+
+sacar(100)
